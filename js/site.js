20c5bda9ca0ffb625235e0627b51a2afe12a0427
michaelabioye <michaelabioye77@gmail.com>
2025-04-23 17:46:43 -0400

    added events to site.js

--- js/site.js
+++ js/site.js
@@ -1,21 +1,65 @@
-function displayMessage() {
-
-    //get the <input/> element from html. you get the element by adding = 
-    // document to your javascript
-    let inputElement = document.getElementById('messageinput');
-
-
-    //then get the text out of the input element 
-    let message = inputElement.value;
-
-    //then use alert() to show the text that was typed
-    Swal.fire(
-        {
-            backdrop: false,
-            title: 'NexTrack',
-            text: message,
-        }
-    )
-
-}
-
+const DEFAULT_EVENTS = [
+    {
+        event: "ComicCon",
+        city: "New York",
+        state: "New York",
+        attendance: 240000,
+        date: "06/01/2017",
+    },
+    {
+        event: "ComicCon",
+        city: "New York",
+        state: "New York",
+        attendance: 250000,
+        date: "06/01/2018",
+    },
+    {
+        event: "ComicCon",
+        city: "New York",
+        state: "New York",
+        attendance: 257000,
+        date: "06/01/2019",
+    },
+    {
+        event: "ComicCon",
+        city: "San Diego",
+        state: "California",
+        attendance: 130000,
+        date: "06/01/2017",
+    },
+    {
+        event: "ComicCon",
+        city: "San Diego",
+        state: "California",
+        attendance: 140000,
+        date: "06/01/2018",
+    },
+    {
+        event: "ComicCon",
+        city: "San Diego",
+        state: "California",
+        attendance: 150000,
+        date: "06/01/2019",
+    },
+    {
+        event: "HeroesCon",
+        city: "Charlotte",
+        state: "North Carolina",
+        attendance: 40000,
+        date: "06/01/2017",
+    },
+    {
+        event: "HeroesCon",
+        city: "Charlotte",
+        state: "North Carolina",
+        attendance: 45000,
+        date: "06/01/2018",
+    },
+    {
+        event: "HeroesCon",
+        city: "Charlotte",
+        state: "North Carolina",
+        attendance: 50000,
+        date: "06/01/2019",
+    },
+];
